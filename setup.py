22fb93af3db9b92936bf1cb83e67d1df23fb3a40
shayanalvand <shayaan.alvandniya22@gmail.com>
2022-12-04 23:45:48 +0100

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='snowflake',
+    version='0.0.1',
+    author='shayan',
+    author_email='shayaan.alvandniya22@gmail.com',
+    url='',
+    license='LICENSE',
+    packages=find_packages(),
+    description='Let it snow!'
+)
