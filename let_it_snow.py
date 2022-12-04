57c4ff66f9b92da1dba8069e5b4bcadf8bfb6bfc
shayanalvand <shayaan.alvandniya22@gmail.com>
2022-12-04 23:32:49 +0100

    choosing color randomly added to the code

--- let_it_snow.py
+++ let_it_snow.py
@@ -15,16 +15,19 @@ def main(speed=0, bg_color="grey"):
  
     """TODO: define different colors here"""
     colors = ('black','red','blue','pink','yellow','purple','white')
-
+    my_color = np.random.randint(0,len(colors))
+    myTurtle.color(colors[my_color])
+    
 
     for _ in range(10):
         # define some params
         size = 18
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
+        
+        """TODO: set snowflake color here (one of the colors defined above)"""
         my_color = np.random.randint(0,len(colors))
         myTurtle.color(colors[my_color])
-        """TODO: set snowflake color here (one of the colors defined above)"""
-
+    
         # Go to the start position of the snowflake
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
